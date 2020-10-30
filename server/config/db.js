312acf8512cdb580d87d4cf385604b6e91d5ef3e
Ibrahim61296 <ipatel099@gmail.com>
2020-10-30 17:11:53 -0400

    Updated commit for MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://ibrahim:K0dkfs91hBNsb1Um@mongodbserver.92nes.mongodb.net/books229?retryWrites=true&w=majority"
 };
